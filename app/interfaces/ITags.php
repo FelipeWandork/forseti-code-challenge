2c89f86c38680e677bbd9e805a37e6649fc1fb93
FelipeWandork <wandork@gmail.com>
2022-05-26 21:36:47 -0300

    Atualização do README e arquivo .sql

--- app/interfaces/ITags.php
+++ app/interfaces/ITags.php
@@ -6,5 +6,7 @@ interface ITags {
   public function toSaveNews( $titles, $links, $dates, $hours );
 
   public function toUpdateNews( $titles, $links, $dates, $hours );
-  
+
+  public function select( $where, $order, $limit, $fields );
+
 }
--- app/model/TagsToCSV.php
+++ app/model/TagsToCSV.php
@@ -3,11 +3,11 @@ namespace app\model;
 
 class TagstoCSV implements ITags {
 
-  function __construct(argument)  {
+  function __construct(argument) {}
 
-  }
+  public function toSaveNews( $titles, $links, $dates, $hours ) {}
 
-  public function toSaveNews($titles, $links, $dates, $hours) {
+  public function toUpdateNews( $titles, $links, $dates, $hours ) {}
 
-  }
+  public function select( $where, $order, $limit, $fields ){}
 }
